248594509f09a8831020931f2c6dccd9486be4ec
Justin Heath <just.coder@outlook.com>
2024-05-22 17:56:16 +0100

    reset project

--- Task_2/app.js
+++ Task_2/app.js
@@ -1,14 +1,8 @@
 updateBG = () => {
   // console.log(document.querySelector("body").style.backgroundColor);
   const x = document.querySelector("body").style.backgroundColor;
-  document.querySelector("body").style.backgroundColor = (x === "azure") ? "lightgoldenrodyellow" : "azure";
-}
-
-updateOleksandrBG = () => {
-  // console.log(document.querySelector("body").style.backgroundColor);
-  const x = document.getElementById("oleksandrBlock").style.backgroundColor;
-  document.getElementById("oleksandrBlock").style.backgroundColor = (x === "blue") ? "yellow" : "blue";
-}
+  document.querySelector("body").style.backgroundColor =
+    x === "azure" ? "lightgoldenrodyellow" : "azure";
+};
 
 document.querySelector("#myBtn").addEventListener("click", updateBG);
-document.querySelector("#oleksandrBtn").addEventListener("click", updateOleksandrBG);
