8323deec0c0edf71b65af086b97fe0c762e4a67d
Justin Heath <just.coder@outlook.com>
2024-05-29 19:33:51 +0100

    updates

--- Task_2/app.js
+++ Task_2/app.js
@@ -2,7 +2,7 @@ updateBG = () => {
   // console.log(document.querySelector("body").style.backgroundColor);
   const x = document.querySelector("body").style.backgroundColor;
   document.querySelector("body").style.backgroundColor =
-    x === "azure" ? "lightgoldenrodyellow" : "azure";
+    x === "azure" ? "firebrick" : "azure";
 };
 
 document.querySelector("#myBtn").addEventListener("click", updateBG);
